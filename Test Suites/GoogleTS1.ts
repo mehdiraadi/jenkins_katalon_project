d53a9c0429ee9ef0834ba5ec5dc071be9eef2970
Jasmi Shaheer <jasmi@doctrin.se>
2019-06-05 11:34:19 +0200

    added the project

---
+++ Test Suites/GoogleTS1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>GoogleTS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>682614aa-c40f-41aa-b826-fb6e17fe7f80</testSuiteGuid>
+</TestSuiteEntity>
